afc11713ad20b19b240096d4b19d38098ca2ab24
Halborzi <122056708+Halborzi@users.noreply.github.com>
2023-01-06 13:41:31 +0330

    Method and classes got arranged

--- src/domain/CSE.java
+++ src/domain/CSE.java
@@ -1,24 +1,27 @@
 package domain;
 import java.util.Date;
+import domain.exceptions.EnrollmentRulesViolationException;
+import java.util.List;
 
-public class CSE {
+
+public class CourseSectionExamDate {
 	private Course course;
 	private int section;
 	private Date examDate;
 
-	public CSE(Course course) {
+	public CourseSectionExamDate(Course course) {
 		this.course = course;
 		this.section = 1;
 		this.examDate = null;
 	}
 
-	public CSE(Course course, Date examDate) {
+	public CourseSectionExamDate(Course course, Date examDate) {
 		this.course = course;
 		this.section = 1;
 		this.examDate = examDate;
 	}
 
-	public CSE(Course course, Date examDate, int section) {
+	public CourseSectionExamDate(Course course, Date examDate, int section) {
 		this.course = course;
 		this.section = section;
 		this.examDate = examDate;
@@ -37,4 +40,27 @@ public class CSE {
 	}
 
 	public int getSection() { return section; }
+
+	public boolean checkExamTimeConflict(CourseSectionExamDate c) {
+		return getExamTime().equals(c.getExamTime());
+	}
+
+	public void checkDuplicateEnrollment(List<CourseSectionExamDate> courses) throws EnrollmentRulesViolationException {
+		for (CourseSectionExamDate courseSectionExamDate : courses) {
+			if (this == courseSectionExamDate)
+				continue;
+			if (this.getCourse().equals(courseSectionExamDate.getCourse()))
+				throw new EnrollmentRulesViolationException(String.format("%s is requested to be taken twice",
+						this.getCourse().getName()));
+		}
+	}
+
+	public void checkCourseExamTimeConflicts(List<CourseSectionExamDate> courses) throws EnrollmentRulesViolationException {
+		for (CourseSectionExamDate courseSectionExamDate : courses) {
+			if (this == courseSectionExamDate)
+				continue;
+			if (this.checkExamTimeConflict(courseSectionExamDate))
+				throw new EnrollmentRulesViolationException(String.format("Two offerings %s and %s have the same exam time",
+						this, courseSectionExamDate));		}
+	}
 }
